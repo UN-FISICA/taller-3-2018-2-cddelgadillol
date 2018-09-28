b2cb36090dd6d72173f06ad32de157262a3ad856
cddelgadillol <42813059+cddelgadillol@users.noreply.github.com>
2018-09-27 21:45:46 -0500

    Add files via upload
    mas completo que el anterior

---
+++ myfloat_func.py
@@ -0,0 +1,479 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8-*-
+
+sf1=["+",5]
+sf2=["+",2,3,4]
+sf3=["+",5,3,4]
+sf4=["+",2,3,4,8,6,5,3,1,2]
+sf5=["-",4]
+sf6=["-",3,3,4]
+sf7=["-",8,3,4]
+sf8=["-",2,3,4,8,6,5,3,9,2]
+
+prueba=["+",1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2]
+prue=[2,3,4,8,6,5,3,9,2]
+pruebab=["-",9,7,3,8,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2,1,5,3,2]
+prueb=[2,3,4,2]
+
+af2=[4,]
+af3=[3,3,4]
+af4=[8,3,4]
+af5=[2,3,4,8,6,5,3,9,2]
+tf6=(sf3,af2)
+tf7=(sf2,af3)
+
+tf8=(prueba,prue)
+tf9=(pruebab,prueb)
+
+global estado
+global signo
+global inicial
+global corrimiento
+global uno
+global david
+global davi
+corrimiento=0
+ceros=["+",0]
+uno=[0,0]
+inicial=(ceros,uno)
+estado=" "
+signo="a"
+david=" "
+davi=" "
+def imprimir(a):
+	g=0
+	if david=="b=a" and davi=="tres" :
+		mensaje="0"
+	else:
+		mensaje=a[0][0]
+		while g<len(a[0])-1:
+			mensaje += str( a[0][g+1] )
+			g=g+1
+		g=0
+		mensaje +="."
+		while g<len(a[1]):
+			mensaje += str( a[1][g] )
+			g=g+1
+	print (mensaje)
+	return mensaje
+def desenca(a):
+	g=0
+	while g<len(a[0])-1:
+		mensaje += str( a[0][g+1] )
+		g=g+1
+	g=0
+	mensaje +="."
+	while g<len(a[1]):
+		mensaje += str( a[1][g] )
+		g=g+1
+	mensaje=float(mensaje)
+	return mensaje
+def quitarc(a):
+	
+	 entero=len(a[0])
+	 decimal=len(a[1])
+	 g=0
+	 while g<entero-1:
+		 aux=a[0][1]
+		 if aux==0:
+			 a[0].pop(1)
+		 else:
+			 g=entero
+		 g=g+1
+	 g=decimal-1
+	 while g>0:
+		 aux=a[1][g]
+		 if aux==0:
+			 a[1].pop(g)
+		 else:
+			 g=0
+		 g=g-1
+	 g=0
+	 return a
+def completar(a,b):
+	entero=len(a[0])
+	decimal=len(a[1])
+	entero2=len(b[0])
+	decimal2=len(b[1])
+	po=a[0]
+	pb=a[1]
+	if entero<entero2:
+		g=0;
+		resta=entero2-entero
+		while g<resta:
+			po.insert(1,0)
+			g=g+1;
+	if decimal<decimal2:
+		g=0;
+		resta=decimal2-decimal
+		while g<resta:
+			pb.append(0)
+			g=g+1
+		g=0
+	a=[po,pb]
+	return a
+def comparacion(a, b):
+	global david
+	a=quitarc(a)
+	b=quitarc(b)
+	entero=len(a[0])
+	decimal=len(a[1])
+	kll=50
+	entero2=len(b[0])
+	decimal2=len(b[1])
+	
+	if entero>entero2:
+		respuesta="a>b"
+	else:
+		if entero2>entero:
+			respuesta="b>a"
+		else:
+			g=0
+			while g<entero2-1:
+				if a[0][g+1]==b[0][g+1]:
+					kll=9
+				else:
+					kll=23
+					if a[0][g+1]>b[0][g+1]:
+						respuesta="a>b"
+					else:
+						respuesta="b>a"
+					g=entero2
+				
+				g=g+1
+		g=0
+		if kll<20:
+			a=completar(a,b)
+			b=completar(b,a)
+			entero2=len(b[0])
+			decimal2=len(b[1])
+			while g<decimal:
+				if a[1][g]==b[1][g]:
+					kll=99
+				else:
+					kll=23
+					g=decimal
+					if a[1][g]>b[1][g]:
+						respuesta="a>b"
+					else:
+						respuesta="b>a"
+				g=g+1
+		if kll==99:
+			respuesta="b=a"
+	david=respuesta
+	return respuesta
+def sign(a,b,op,c,d):
+	global estado
+	global signo
+	global inicial
+	respuesta=inicial;
+	if op=="+":
+		if a=="+" and b=="+":
+			estado="suma"
+			signo="+"
+		else:
+			if a=="-" and b=="-":
+				estado="suma"
+				signo="-"
+			else:
+				if a=="-" and b=="+":
+					ff=comparacion(c,d)
+					if ff=="a>b":
+						estado="resta"
+						signo="-"
+						respuesta=resta(c,d)
+					else :
+						estado="resta"
+						signo="+"
+						respuesta=resta(d,c)
+						
+				else:
+					ff=comparacion(c,d)
+					if ff=="a>b":
+						estado="resta"
+						signo="+"
+						respuesta=resta(c,d)
+					else :
+						estado="resta"
+						signo="-"
+						respuesta=resta(d,c)
+	if op=="-":
+		if a=="-" and b=="+":
+			estado="suma"
+			signo="-"
+			respuesta=suma(c,d)
+		else:
+			if a=="+" and b=="-":
+				estado="suma"
+				signo="+"
+				respuesta=suma(c,d)
+			else:
+				if a=="-" and b=="-":
+					ff=comparacion(c,d)
+					if ff=="a>b":
+						estado="resta"
+						signo="-"
+						respuesta=resta(c,d)
+						estado=" "
+					else :
+						estado="resta"
+						signo="+"
+						respuesta=resta(d,c)
+						estado=" "
+				else:
+					ff=comparacion(c,d)
+					if ff=="a>b":
+						estado="resta"
+						signo="+"
+						respuesta=resta(c,d)
+						estado=" "
+					else :
+						estado="resta"
+						signo="-"
+						respuesta=resta(d,c)
+						estado=" "
+	if op=="*":
+		estado="*"
+		if (a=="+" and b=="+") or (a=="-" and b=="-"):
+			signo="+"
+		else:
+			signo="-"
+	if op=="/":
+		estado="/"
+		if (a=="+" and b=="+") or (a=="-" and b=="-"):
+			signo="+"
+		else:
+			signo="-"
+	
+	return respuesta
+def suma(a, b):
+	global estado
+	global signo
+	global inicial
+	if estado==" ":
+		respuesta=sign(a[0][0],b[0][0],"+",a,b)
+		g=0
+	if estado=="suma":
+		g=0
+		a=completar(a,b)
+		b=completar(b,a)
+		respuesta=inicial
+		respuesta=completar(respuesta,a)
+		ente=len(a[0])
+		deci=len(a[1])
+		residuo=0
+		g=deci-1
+		while g>-0.001:
+			respuesta[1][g]=a[1][g]+b[1][g]+residuo
+			if respuesta[1][g]>9:
+				intermedio=respuesta[1][g]/10
+				respuesta[1][g]=respuesta[1][g]%10
+				residuo=int(intermedio)
+			else:
+				residuo=0
+			g=g-1
+				
+		g=ente-1
+		while g>0:
+			respuesta[0][g]=a[0][g]+b[0][g]+residuo
+			if respuesta[0][g]>9:
+				intermedio=respuesta[0][g]/10
+				respuesta[0][g]=respuesta[0][g]%10
+				residuo=int(intermedio)
+				
+			else:
+				residuo=0
+			g=g-1
+		g=0
+		respuesta[0][0]=signo
+		if residuo>0:
+				respuesta[0].insert(1,residuo)
+		estado=" "
+	estado=" "
+	return respuesta	
+def resta(a, b):
+	
+	global estado
+	global signo
+	global inicial
+	global davi
+	comparacion(a,b)
+	respuesta=inicial
+	
+	if estado==" ":
+		respuesta=sign(a[0][0],b[0][0],"-",a,b)
+		g=0
+		
+	if estado=="resta":
+		davi="tres"
+		g=0
+		a=completar(a,b)
+		b=completar(b,a)
+		respuesta=inicial
+		respuesta=completar(respuesta,a)
+		a=completar(a,respuesta)
+		b=completar(b,respuesta)
+		ente=len(a[0])
+		deci=len(a[1])
+		residuo=0
+		g=deci-1
+		while g>-0.001:
+			respuesta[1][g]=a[1][g]-b[1][g]-residuo
+			if respuesta[1][g]<0:
+				respuesta[1][g]=respuesta[1][g]+10
+				residuo=1
+			else:
+				residuo=0
+			g=g-1
+				
+		g=ente-1
+		while g>0:
+			respuesta[0][g]=a[0][g]-b[0][g]-residuo
+			if respuesta[0][g]<0:
+				respuesta[0][g]=respuesta[0][g]+10
+				residuo=1
+			else:
+				residuo=0
+			g=g-1
+		g=0
+		respuesta[0][0]=signo
+		if residuo>0:
+			
+				respuesta[0].insert(1,"error")
+	estado=" "
+	return respuesta
+def pasaraunosolo(a):
+	entero=len(a[0])
+	decimal=len(a[1])	
+	g=0
+	while g<decimal:
+		a[0].append(a[1][0])
+		a[1].pop(0)
+		g=g+1
+	return a
+def vnew(a,tamaño,e):
+	if e>0:
+		a[0].pop(0)
+	g=0
+	while g<tamaño:
+		a[0].append(0)
+		g=g+1
+		return a
+def recuf(a,b):
+	global inicial
+	entero=len(a[0])
+	decimal=len(a[1])
+	c=a[0][entero-1]
+	d=b[0][entero-1]
+	lespesta=inicial;
+	tespesta=inicial;
+	lespesta=completar(lepesta,a);
+	tespesta=completar(tepesta,b);
+	a[0].pop(entero-1)	
+	b[0].pop(entero-1)	
+	g=entero-2
+	re1=0
+	re2=0
+
+	while g>0:
+		lespesta[0][g]=a[0][g]*d+re1
+		if lespesta[0][g]>9:
+			interm=lespesta[0][g]/10
+			re1=int(interm)
+			mut=lespesta[0][g]%10
+			lespesta[0][g]=mut
+		else:
+			re1=0
+		tespesta[0][g]=b[0][g]*c+re2
+		if tespesta[0][g]>9:
+			interm=tespesta[0][g]/10
+			re2=int(interm)
+			mut=tespesta[0][g]%10
+			tespesta[0][g]=mut
+		else:
+			re2=0
+		g=g-1
+	if re1>0:
+		lespesta.insert(1,re1)
+		tespesta.insert(1,0)
+	if re2>0:
+		tespesta.insert(1,re2)
+		lespesta.insert(1,0)
+	reptu=suma(tespesta,lespesta)
+	return reptu
+def multiplicacion(a, b):
+	global signo
+	global estado
+	global corrimiento
+	global inicial
+	global uno
+	a=quitarc(a)
+	b=quitarc(b)
+	signo=sign(a[0][0],b[0][0],"*",a,b)
+	
+	entero=len(a[0])
+	decimal=len(a[1])
+	
+	entero2=len(b[0])
+	decimal2=len(b[1])
+	
+	corrimiento=decimal+decimal2
+	a=pasaraunosolo(a)
+	b=pasaraunosolo(b)
+	a=completar(a,b)
+	b=completar(b,a)
+	
+	entero=len(a[0])
+	decimal=len(a[1])
+	solucion=inicial
+	solucion=vnew(solucion,entero*2,4)
+	tama=len(solucion[0])
+	A=a[0]
+	B=b[0]
+	aux=0
+	jF=inicial
+	g=entero-1
+	multi=inicial
+	ret=inicial
+	while g>0:
+		intermedi=A[g]*B[g]/10
+		residuo=int(intermedi)
+		multip=A[g]*B[g]%10
+		multi[0][1]=multip
+		Nf=suma(jF,multi)
+		actualice=len(Nf[0])
+		solucion[0][tama-1-2*aux]=Nf[0][actualice-1]
+		Nf[0].pop(actualice-1)
+		jF=recuf(a,b)
+		jF=suma(jF,Nf)
+		ret[0][1]=residuo
+		jF=suma(jF,ret)
+		actualice=len(jF[0])
+		solucion[0][tama-2-2*aux]=jF[0][actualice-1]
+		jF[0].pop(actualice-1)
+		aux=aux+1
+		g=g-1
+	solucion=quitarc(solucion)
+	if residuo>0:
+		solucion[0].insert(1,residuo)
+	print(corrimiento)
+	aux=0
+	estado=" "
+	return solucion
+def division(a, b):
+	
+	
+    pass
+if __name__ == "__main__":
+	
+	resa=suma(tf8,tf9)
+	print(tf8)
+	print(tf9)
+	imprimir(resa)
+	res=resta(tf8,tf9)
+	print(" ")
+	imprimir(res)
+	
+	
+	
+
